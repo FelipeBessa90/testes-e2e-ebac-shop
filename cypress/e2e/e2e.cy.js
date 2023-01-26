f5948173c81685423f96e4a6252205e93620f9ef
Felipe Bessa <felipe.bessa1990@gmail.com>
2023-01-26 14:59:15 -0300

    Atualizacao
    26/01/2023

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
-var faker = require('faker-br');
+import EnderecoPage from '../support/page_objects/endereco.page'
+
+const dadosEndereco = require('../fixtures/endereco.json')
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -11,56 +13,52 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br')
+        cy.visit('minha-conta/')
     });
     afterEach(() => {
         cy.screenshot()
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        var quantidade = 4
-        let nomefaker = faker.name.firstName()
-        let sobrenomeFaker = faker.name.lastName()
-        let emailfaker = faker.internet.email(nomefaker, sobrenomeFaker)
-          
-        cy.get('.icon-user-unfollow').click()
-        cy.get('#reg_email').type(emailfaker)
-        cy.get('#reg_password').type('!teste@teste$')
-        cy.get(':nth-child(4) > .button').click()
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(nomefaker)
-        cy.get('#account_last_name').type(sobrenomeFaker)
-        cy.get('.woocommerce-Button').click()
-        cy.get('.woocommerce-MyAccount-navigation-link--edit-address > a').click()
-        cy.get(':nth-child(2) > .title > .edit').click()
-        cy.get('#shipping_first_name').type(nomefaker)
-        cy.get('#shipping_last_name').type(sobrenomeFaker)
-        cy.get('#shipping_company').type('FAKER 10 LTDA')
-        cy.get('#select2-shipping_country-container').click().type('Brasil').get('[aria-selected="true"]').click()
-        cy.get('#shipping_address_1').clear().type('Rua das Ruas')
-        cy.get('#shipping_address_2').clear().type(100)
-        cy.get('#shipping_city').clear().type('São Paulo')
-        cy.get('#select2-shipping_state-container').click().type('São Paulo'+ '{enter}')
-        cy.get('#shipping_postcode').clear().type('05594-100')               
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-        cy.get('[class="product-block grid"]').eq(1).click()
-        cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Green').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.woocommerce-message > .button').click()
-        cy.get('.checkout-button').click()
-        cy.get('#billing_company').type('FAKER 10 LTDA')
-        cy.get('#billing_address_1').type('Rua das Ruas N100')
-        cy.get('#billing_address_2').type('casa')
-        cy.get('#billing_city').type('São Paulo')
-        cy.get('#billing_postcode').type('05594-100') 
-        cy.get('#billing_phone').clear().type('11 94735-5227')
-        cy.get('#terms').click()
-        cy.get('#place_order').click()
-        
-        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
-    });
+        var quantidade = 2
+        //var Tamanho = M
+        cy.fixture('perfil').then((dados) => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha)
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.page-title').should('contain', 'Minha conta')
+            EnderecoPage.editarEnderecoFaturamento(
+                dadosEndereco[1].nome,
+                dadosEndereco[1].sobrenome,
+                dadosEndereco[1].empresa,
+                dadosEndereco[1].pais,
+                dadosEndereco[1].endereco,
+                dadosEndereco[1].numero,
+                dadosEndereco[1].cidade,
+                dadosEndereco[1].estado,
+                dadosEndereco[1].cep,
+                dadosEndereco[1].celular,
+                dadosEndereco[1].email
+            )
+            cy.get('#primary-menu > .menu-item-629 > a').click()
+            cy.get('[class="product-block grid"]')
+                .eq(1).click()
+            cy.get('.button-variable-item-M').click()
+            cy.get('.button-variable-item-Green').click()
+            cy.get('.input-text').clear().type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            cy.get('.woocommerce-message > .button').click()
+            cy.get('.checkout-button').click()
+            cy.get('#terms').click().type('{enter}')
+      
+            
+            cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
+
+        })
+
+
+    })
 
 
 })
